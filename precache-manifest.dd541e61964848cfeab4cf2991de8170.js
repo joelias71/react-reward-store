8af092bd7c05e5aac25537a11034842ede1d6205
Jorge Elias <jorgea.eliasl7@gmail.com>
2020-10-03 21:09:08 -0500

    Updates

---
+++ precache-manifest.dd541e61964848cfeab4cf2991de8170.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd723f5f7f0787d487be12329ee28a16",
+    "url": "/react-reward-store/index.html"
+  },
+  {
+    "revision": "22ab17284df2c87939f2",
+    "url": "/react-reward-store/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "77583f30b3fc999d0201",
+    "url": "/react-reward-store/static/js/2.2c635f5e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-reward-store/static/js/2.2c635f5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22ab17284df2c87939f2",
+    "url": "/react-reward-store/static/js/main.0658f486.chunk.js"
+  },
+  {
+    "revision": "bc41c577b4f90af0d6e6",
+    "url": "/react-reward-store/static/js/runtime-main.b9c685af.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/react-reward-store/static/media/logo.ee7cd8ed.svg"
+  }
+]);
