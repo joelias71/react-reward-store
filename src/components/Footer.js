7cc60b77353082cd6f3df17f0560249221777ad8
Jorge Elias <jorgea.eliasl7@gmail.com>
2020-10-09 19:37:44 -0500

    feat(footer) Add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div className='footer'>
+            <p>Created by Jorge Elias | 2020</p>
+        </div>
+    )
+}
+
+export default Footer
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../components/Header'
 import Banner from '../components/Banner'
+import Footer from '../components/Footer'
 
 function Home() {
     return (
         <main>
             <Header />
             <Banner />
+            <Footer />
         </main>
     )
 }
