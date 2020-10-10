7160600e928f2cf86081fa6d7f8ddca9f265ce63
Jorge Elias <jorgea.eliasl7@gmail.com>
2020-10-10 13:54:09 -0500

    feat(axios) Add axios to the project

---
+++ src/data/axios.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+/** base url */
+const instance = axios.create({
+    baseURL: 'https://coding-challenge-api.aerolab.co',
+    headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZWRkOWU5OTQ0NGZlNDAwNmRhOTkyNGQiLCJpYXQiOjE1OTE1ODIzNjF9.-f40dyUIGFsBSB_PTeBGdSLI58I21-QBJNi9wkODcKk'
+    }
+})
+
+export default instance
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from '../components/Header'
 import Banner from '../components/Banner'
 import Footer from '../components/Footer'
+import axios from '../data/axios'
 
 function Home() {
+    
+    useEffect(() => {
+        axios.get('products')
+            .then(response => console.log(response))
+            .catch(error => console.log(error))
+    })
+
     return (
         <main>
             <Header />
