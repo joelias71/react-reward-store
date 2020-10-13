d8aeaf2649d2bc4c4939bd5c335b728c8d8048f2
Jorge Elias <jorgea.eliasl7@gmail.com>
2020-10-12 21:00:38 -0500

    feat(hover) Hover effect to product list

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,10 +6,17 @@ function Card({ product }) {
 
     return (
         <div className='card'>
-            <img src={product.img.url} alt={product.name} />
-            <hr />
-            <p className='card__category' >{product.category}</p>
-            <p className='p.card__name' >{product.name}</p>
+            <div className='card__inner'>
+                <div className='card__inner_normal'>
+                    <img src={product.img.url} alt={product.name} />
+                    <hr />
+                    <p className='card__category' >{product.category}</p>
+                    <p className='p.card__name' >{product.name}</p>
+                </div>
+                <div className='card__inner_hover'>
+                    
+                </div>
+            </div>
         </div>
     )
 }
