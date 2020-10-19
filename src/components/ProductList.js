bcb48599f20d198d928e000251adaf2ba4a91684
Jorge Elias <jorgea.eliasl7@gmail.com>
2020-10-19 16:46:25 -0500

    feat(pagination) Add pagination to product list

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,10 +1,23 @@
 import React, { useState, useEffect } from 'react'
 import axios from '../data/axios'
 import Card from './Card'
+import Pagination from '@material-ui/lab/Pagination'
+import usePagination from './usePagination'
 
 function ProductList() {
 
     const [ products, setProducts ] = useState([])
+    const [page, setPage] = useState(1)
+    const PER_PAGE = 12
+
+    const count = Math.ceil(products.length / PER_PAGE)
+    const _DATA = usePagination(products, PER_PAGE)
+
+    const handleChange = (e, p) => {
+        setPage(p)
+        _DATA.jump(p)
+    }
+
 
     useEffect(() => {
         axios.get('products')
@@ -13,12 +26,23 @@ function ProductList() {
             }).catch(error => console.log(error))
     },[])
 
-    const cardProductList = products.map(prod => <Card key={prod._id} product={prod} />)
+    const cardProductList = _DATA.currentData().map(prod => <Card key={prod._id} product={prod} />)
 
     return (
-        <div className='productListContainer' >
-            { cardProductList }
-        </div>
+        <>
+            <div className='productListContainer' >
+                { cardProductList }
+            </div>
+            <div className='productPagination' >
+                <Pagination 
+                    count={count}
+                    size="large" 
+                    page={page}
+                    shape="rounded"
+                    onChange={handleChange}
+                />
+            </div>
+        </>
     )
 }
 
